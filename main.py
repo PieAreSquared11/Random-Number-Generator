7f92eb2953919e3207d1f30af92b229d424a861c
unknown <jbwpsa11@outlook.com>
2024-11-07 17:38:05 -0500

    Updated main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from datetime import datetime
+import round
 
 date = datetime.now()
 
 result = date.year + date.month + date.day + date.hour + date.minute + date.second + date.microsecond
+
+result = round.round_to_place_value(result, highest=True)
 
 print(result)
--- round.py
+++ round.py
@@ -1,17 +1,29 @@
 import math
 
-def round_to_place_value(value, p_val):
-    if type(math.log(p_val)) != "float":
-        value /= p_val
+def round_to_place_value(value, p_val=0, highest=False):
+    if not highest:
+        if type(math.log(p_val)) != "float":
+            value /= p_val
 
-        value = round(value)
+            value = round(value)
 
-        value *= p_val
-    
-        return value
+            value *= p_val
+        
+            return value
+        else:
+            print("unexpected value")
+            raise ValueError
     else:
-        print("unexpected value")
-        raise ValueError
+        str_val = str(value)
+
+        if "." in str_val:
+            p_val = 10 ** (len(str_val.split(".")[0]) - 1)
+
+            return round_to_place_value(value, p_val=p_val)
+        else:
+            p_val = 10 ** (len(str_val) - 1)
+
+            return round_to_place_value(value, p_val=p_val)
         
 
-print(round_to_place_value(2297, 100))
+# EXAMPLE: print(round_to_place_value(2297.65, highest=True))
