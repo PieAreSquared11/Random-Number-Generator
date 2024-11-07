f8d43320227f5fff6f2b416d7e200e83e91836cd
unknown <jbwpsa11@outlook.com>
2024-11-07 17:39:34 -0500

    Updated main.py

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from datetime import datetime
 import round
 
-date = datetime.now()
+def set_p_value(value, p_val):
+    str_val = str(value)
 
-result = date.year + date.month + date.day + date.hour + date.minute + date.second + date.microsecond
+    if "." in str_val:
+        p_val = 10 ** len(str_val.split(".")[0])
 
-result = round.round_to_place_value(result, highest=True)
+        return value / p_val
+    else:
+        p_val = 10 ** len(str_val)
 
-print(result)
+        return value / p_val
+
+def rand():
+    date = datetime.now()
+
+    result = abs(date.year - date.month - date.day + date.hour - date.minute + date.second + date.microsecond)
+
+    result = round.round_to_place_value(result, highest=True)
+
+    result = set_p_value(result, 0.1)
+
+    return result
